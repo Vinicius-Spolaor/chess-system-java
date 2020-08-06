37e678c5786098fe48da5f20d46a60b0b11e2ba1
Vinicius-Spolaor <vinisspy@gmail.com>
2020-08-06 16:51:31 -0300

    Handling exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -32,6 +32,12 @@ public class UI {
 
 	// Methods
 
+	// https://stackoverflow.com/questions/2979383/java-clear-the-console
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+	
 	public static ChessPosition readChessPosition(Scanner tec) {
 		try {
 			String s = tec.nextLine();
